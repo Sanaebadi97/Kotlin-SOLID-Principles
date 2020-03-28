65dc2b76a7d0ec466aaeacb4e25612a87a362a59
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-28 08:42:31 -0400

    added 3 interface for 3 methods !

---
+++ app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/academy/ISP/after/OnClickListener.kt
@@ -0,0 +1,10 @@
+package info.sanaebadi.kotlinSolidPrinciples.academy.ISP.after
+
+import android.view.MotionEvent
+import android.view.View
+
+
+interface OnClickListener {
+    fun onClick(v: View?)
+
+}
---
+++ app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/academy/ISP/after/onLongClick.kt
@@ -0,0 +1,9 @@
+package info.sanaebadi.kotlinSolidPrinciples.academy.ISP.after
+
+import android.view.MotionEvent
+import android.view.View
+
+interface OnLongClick {
+    fun onLongClick(v: View?)
+
+}
