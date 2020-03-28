ca88c175e3c01d160282e817e80cc2410b6e8737
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-28 08:39:34 -0400

    added 3 methods from onClickListener for click on button and is wrong !

--- app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/academy/ISP/before/MainActivity.kt
+++ app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/academy/ISP/before/MainActivity.kt
@@ -1,13 +1,33 @@
 package info.sanaebadi.kotlinSolidPrinciples.academy.ISP.before
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import info.sanaebadi.kotlinSolidPrinciples.R
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val create: Button = findViewById(R.id.create)
+
+        create.setOnClickListener{
+            // assume this is a todo based app.
+            myDatabase.createTask(...)
+
+            fun onLongClick(v: View?){
+
+                // do nothing, we're not long clicking
+            }
+
+            fun onTouch(v: View?, event: MotionEvent?){
+                // do nothing, we're not worried about touch
+            }
+        }
     }
 }
