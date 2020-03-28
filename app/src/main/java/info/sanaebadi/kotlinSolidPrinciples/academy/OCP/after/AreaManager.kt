f84962c099d6b0e2ab8ed08d36d3ef58ec853d89
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-28 07:50:18 -0400

    added  some comment in AreaManager class

--- app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/academy/OCP/after/AreaManager.kt
+++ app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/academy/OCP/after/AreaManager.kt
@@ -1,5 +1,12 @@
 package info.sanaebadi.kotlinSolidPrinciples.academy.OCP.after
 
+
+/**
+ *
+ * We’ve made changes to the AreaManager that allow it to be closed for modification but open for extension. If we need to add a new shape
+ * , such as an octagon, the AreaManager will not need to be changed because it is open for extension through the Shape interface
+ */
+
 class AreaManager {
 
     fun calculateArea(vararg shapes: ArrayList<Any?>?): Double {
