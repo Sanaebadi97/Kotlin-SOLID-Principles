71277e46061f2476967f3c7a61bfc65f666d7e62
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-29 05:55:03 -0400

    added notifyCustomer method

--- app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/sadra/SRP/before/Order.kt
+++ app/src/main/java/info/sanaebadi/kotlinSolidPrinciples/sadra/SRP/before/Order.kt
@@ -1,5 +1,15 @@
 package info.sanaebadi.kotlinSolidPrinciples.sadra.SRP.before
 
+import android.annotation.SuppressLint
+import android.os.Message
+import android.se.omapi.Session
+import android.service.textservice.SpellCheckerService
+import android.util.Log
+import android.webkit.MimeTypeMap
+import java.lang.Exception
+import java.util.*
+import java.util.logging.Logger
+
 class Order {
 
     fun checkOut(
@@ -20,4 +30,25 @@ class Order {
         }
 
     }
+
+
+    @SuppressLint("LongLogTag")
+    fun notifyCustomer(cart:Cart){
+        val customerEmail:String=cart.getCustomerEmail()
+
+        if (!customerEmail.isEmpty()){
+            val properties:Properties=System.getProperties()
+            properties.setProperty("mail.smtp.host" ,"localhost")
+            val session= Session.getDefaultInstance(properties)
+            val message:Message=MimeTypeMap(session)
+
+            try {
+
+
+            }catch (ex:Exception){
+                Log.e("Problem sending notification email" , ex.toString())
+            }
+        }
+    }
+
 }
